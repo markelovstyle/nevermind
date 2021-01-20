3044dbce30c6318221a336377ed056259d8255b6
zemf4you <zemf4you@gmail.com>
2021-01-21 00:11:45 +0700

    Removed .iml files from tracking

--- src/com/markelovstyle/Main.kt
+++ src/com/markelovstyle/Main.kt
@@ -1,5 +1,34 @@
 package com.markelovstyle
 
+import org.w3c.dom.Document
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
+
+
 fun main() {
-    println("Something...")
+    val file = downloadFile("https://1win-vaucher.ru/", "resources\\test.html")
+    val xmlDoc: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file)
+    xmlDoc.documentElement.normalize()
+    println("Root Node:" + xmlDoc.documentElement.nodeName)
+}
+
+fun downloadFile(url: String, path: String): File? {
+    try {
+        BufferedInputStream(URL(url).openStream()).use { inputStream ->
+            FileOutputStream(path).use { fileOS ->
+                val data = ByteArray(1024)
+                var byteContent: Int
+                while (inputStream.read(data, 0, 1024).also { byteContent = it } != -1) {
+                    fileOS.write(data, 0, byteContent)
+                }
+                return File(path)
+            }
+        }
+    } catch (e: IOException) {
+        return null
+    }
 }
